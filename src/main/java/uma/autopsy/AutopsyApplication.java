4fff1f495076f07c1c4149944b5a2b3e5a942422
mohammadsuhail98 <msuhayl@earthlinktele.com>
2024-07-31 19:11:31 +0200

    First commit.

---
+++ src/main/java/uma/autopsy/AutopsyApplication.java
@@ -0,0 +1,13 @@
+package uma.autopsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutopsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uma/autopsy/AutopsyApplicationTests.java
@@ -0,0 +1,13 @@
+package uma.autopsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutopsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
